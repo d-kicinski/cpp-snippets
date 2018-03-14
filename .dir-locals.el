f9337a2f71e0c8ecba3c54beee6fc0b1345a8658
Dawid Kiciński <kicinski.dawid@gmail.com>
2018-03-14 07:26:44 +0100

    boyy :100:

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+
+((nil .
+      (
+       (cmake-ide-project-dir . "~/code/cpp/Cpp17-STL--Cookbook-Snippets")
+       (cmake-ide-build-dir . "~/code/cpp/Cpp17-STL--Cookbook-Snippets/build")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug")
+       (helm-make-build-dir . "build")
+       (helm-make-arguments . "-j7 -s")
+       )))
+
+
